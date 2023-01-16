69146dd6345ae8525b336013c8857f94d6a4f125
Sergey Afonin <afoninsb@yandex.ru>
2023-01-16 21:47:19 +0300

    list users - responses statuses - done

--- foodgram/users/views.py
+++ foodgram/users/views.py
@@ -32,12 +32,19 @@ class UsersViewSet(CreateListRetrieveViewSet):
     def get_permissions(self):
         """Права доступа для GET запросов."""
 
-        print(self.action)
         if self.action in ('create', 'list'):
             self.permission_classes = (AllowAny, )
         elif self.action in ('retrieve', 'me', 'set_password'):
             self.permission_classes = (IsAuthenticated, )
         return super().get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @action(
         detail=False,
@@ -62,7 +69,8 @@ class UsersViewSet(CreateListRetrieveViewSet):
             request.data,
             partial=True
         )
-        serializer.is_valid(raise_exception=True)
-        self.request.user.set_password(request.data["new_password"])
-        self.request.user.save()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        if serializer.is_valid(raise_exception=True):
+            self.request.user.set_password(request.data["new_password"])
+            self.request.user.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
