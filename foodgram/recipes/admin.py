2c1daa8aa260d7d2ac38235cbcea99944835663f
Sergey Afonin <afoninsb@yandex.ru>
2023-01-16 23:18:08 +0300

    import data from csv - done

--- foodgram/recipes/admin.py
+++ foodgram/recipes/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import Ingredient, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """Представление тэгов в админ-панели."""
+
+    list_display = (
+        'name',
+        'slug',
+        'color',
+    )
+    search_fields = ('name',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    """Представление ингредиентов в админ-панели."""
+
+    list_display = (
+        'name',
+        'unit',
+    )
+    search_fields = ('name',)
